0a74e75569aa16aaa05f32f38d3d417323facfef
lokesh-24 <bhupathirajulokeshvarma@gmail.com>
2020-09-13 18:49:41 -0700

    onClick

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
+ 
+  private contact_url:string = 'http://localhost:3000/contact'
+  private notification_url:string = 'http://localhost:3000/notification'
+  private hospitalbeds_url:string = 'http://localhost:3000/dashboard/hospital-beds'
+  private collegebeds_url:string = 'http://localhost:3000/dashboard/medicalcollege-beds'
+  private deceased_url:string = ''
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+   
+  }
+   
+  contact_details(){
+    return this.http.get(this.contact_url);
+  }
+
+  getnotifications()
+  {
+    return this.http.get(this.notification_url);
+  }
+  
+  gethospitalbeds(){
+    return this.http.get(this.hospitalbeds_url); 
+  }
+  getcollegebeds(){
+    return this.http.get(this.collegebeds_url);       
+  }
+  getdeceasedcount(){
+    return this.http.get(this.deceased_url)
+  }
+
 }
